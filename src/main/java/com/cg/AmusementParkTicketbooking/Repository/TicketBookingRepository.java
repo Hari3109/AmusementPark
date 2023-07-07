ac9fe2e407c14685effc14fa02715df475157171
hsarvasu <hsarvasu@inlen6239015097.corp.capgemini.com>
2023-07-07 11:22:39 +0530

    First Commit

---
+++ src/main/java/com/cg/AmusementParkTicketbooking/Repository/TicketBookingRepository.java
@@ -0,0 +1,19 @@
+package com.cg.AmusementParkTicketbooking.Repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.AmusementParkTicketbooking.entities.TicketBooking;
+
+@Repository
+
+public interface TicketBookingRepository extends JpaRepository<TicketBooking,Integer>{
+
+	List<TicketBooking> findActivityByTicketBookingId(int ticketBookingId);
+
+	void findByticketBookingId(int ticketBookingId);
+
+	
+
+}
